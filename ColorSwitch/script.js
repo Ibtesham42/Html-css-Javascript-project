10eff2bf9a75e842e181d3b574d963a2ac1918e8
Ibtesham Akhtar <ibteshamakhtar1@gmail.com>
2023-12-18 14:18:42 +0530

    Color Changer

---
+++ ColorSwitch/script.js
@@ -0,0 +1,59 @@
+const buttons = document.querySelectorAll('.button');
+const body =document.querySelector("body");
+
+buttons.forEach(function (button) {
+    button.addEventListener('click', function(e){
+        if(e.target.id === 'grey')
+        {
+            body.style.backgroundColor = e.target.id;
+
+        }
+
+    });
+
+
+
+
+    button.addEventListener('click', function(e){
+        if(e.target.id === 'red')
+        {
+            body.style.backgroundColor = e.target.id;
+
+        }
+
+    });
+
+    button.addEventListener('click', function(e){
+        if(e.target.id === 'white')
+        {
+            body.style.backgroundColor = e.target.id;
+
+        }
+
+    });
+
+    button.addEventListener('click', function(e){
+        if(e.target.id === 'blue')
+        {
+            body.style.backgroundColor = e.target.id;
+
+        }
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
