5f2c6dc9e79d27801cfcbb48eee3690f25fadae0
Ibtesham Akhtar <ibteshamakhtar1@gmail.com>
2023-12-19 20:19:44 +0530

    Changes

---
+++ ComingSoonPage/script.js
@@ -0,0 +1,35 @@
+
+
+      //Set Time 
+let CountDownDate = new Date("Jan 19, 2024 00:00:00").getTime();
+
+
+// logic n interval funcion used
+let x = setInterval(() => {
+    let now = new Date().getTime();
+    let distance = CountDownDate - now;
+
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+
+
+    //After Launching times ends
+
+    if(distance < 0)
+    {
+        clearInterval(x);
+        document.getElementById("days").innerHTML = "00";
+        document.getElementById("hours").innerHTML = "00";
+        document.getElementById("minutes").innerHTML = "00";
+        document.getElementById("seconds").innerHTML = "00";
+    }
+    
+}, 1000);
